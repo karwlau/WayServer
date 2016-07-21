9a3e6877b098d20e58800ae887aa9c677661a59b
karwlau <syaojose@163.com>
2016-07-21 14:13:44 +0800

    page base

---
+++ WayServer/com/way/framework/model/src/main/java/com/way/framework/model/Pagination.java
@@ -0,0 +1,43 @@
+package com.way.framework.model;
+
+import java.util.List;
+
+public class Pagination<T> {
+	private int pageNo;
+	private int pageSize;
+	private int totalNum;
+	private List<T> list;
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public int getTotalNum() {
+		return totalNum;
+	}
+
+	public void setTotalNum(int totalNum) {
+		this.totalNum = totalNum;
+	}
+
+	public List<T> getList() {
+		return list;
+	}
+
+	public void setList(List<T> list) {
+		this.list = list;
+	}
+
+}
---
+++ WayServer/com/way/framework/model/src/main/java/com/way/framework/model/orm/interceptor/PageInterceptor.java
@@ -0,0 +1,35 @@
+package com.way.framework.model.orm.interceptor;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.log4j.Logger;
+
+@Intercepts({ @Signature(type = StatementHandler.class, method = "prepare", args = { Connection.class }) })
+public class PageInterceptor implements Interceptor {
+
+	private static Logger logger = Logger.getLogger(PageInterceptor.class);
+
+	@Override
+	public Object intercept(Invocation ivk) throws Throwable {
+
+		return ivk.proceed();
+	}
+
+	@Override
+	public Object plugin(Object arg0) {
+		return Plugin.wrap(arg0, this);
+	}
+
+	@Override
+	public void setProperties(Properties p) {
+
+	}
+
+}
